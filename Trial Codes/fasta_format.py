984e48f909fc033d7b9b81906a8de3095a37eeb1
Riddhi_Goswami <ridhgoswami@gmail.com>
2024-09-30 20:15:22 +0400

    checking

--- Trial Codes/fasta_format.py
+++ Trial Codes/fasta_format.py
@@ -1,10 +1,16 @@
 import csv
 
-csv_file_path = '/Users/bristi/Desktop/Design Project/Working-with-TF/Gm5454/Gm5454_8mers_top_enrichment.txt'
-fasta_file_path = '/Users/bristi/Desktop/Design Project/Working-with-TF/Gm5454/Gm5454.fasta'
+csv_file_path = '/Users/bristi/Desktop/Design Project/Working-with-TF/TFs UniProbe Data/GATA4/GATA4_anti-GST_8mers_top_enrichment.txt'
+fasta_file_path = 'gata(2).fasta'
             
+sequences = []
 with open(csv_file_path, 'r') as input_file, open(fasta_file_path, 'w') as output_file:
         next(input_file)
         for i, line in enumerate(input_file, start=1):
             sequence = line.strip().split('\t')[0]
-            output_file.write(f">Sequence_{i}\n{sequence}\n")
+            sequence = sequence.replace('.','')
+            sequences.append(sequence)
+        
+        sequences = set(sequences)
+        for i in sequences:      
+            output_file.write(f"{i}\n")
